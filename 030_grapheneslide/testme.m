0ee9520b9e4b51058be3f0c692f0ba305020d371
Alberto Otero de la Roza <alberto@fluor.quimica.uniovi.es>
2015-10-05 18:04:00 -0700

    added example: 030_grapheneslide

---
+++ 030_grapheneslide/testme.m
@@ -0,0 +1,9 @@
+#! /usr/bin/octave -q
+
+mol = mol_readxyz("large_00_00.xyz",0);
+rep = mol_ball(mol,:,"H",:,:,[200 200 200]);
+rep = mol_ball(mol,rep,"C",:,:,[10 10 10]);
+rep = mol_stick(mol,rep,"C","H");
+rep = mol_stick(mol,rep,"C","C");
+rep_write_obj(rep,"large.obj");
+rep = rep_setdefaultscene(rep,r,0);
