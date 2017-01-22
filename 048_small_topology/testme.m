f1402b9241478ed3e3d3f89961827c982a84ede1
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2017-01-22 11:22:22 -0800

    added example: 048_small_topology

---
+++ 048_small_topology/testme.m
@@ -0,0 +1,20 @@
+#! /usr/bin/octave -q
+
+mol = mol_readxyz("ch4.xyz");
+rep = representation_();
+rep = mol_ball(mol,rep,{"C","H"},:,-0.2);
+rep = mol_ball(mol,rep,"X.?",1,-0.4);
+rep = rep_setdefaultscene_view3dscene(rep,[7.02,8.55,10.0],[0.86,0.49,0.15],[-0.36,0.78,-0.51],:,:,:,0);
+rep_write_obj(rep,"ch4.obj");
+rep_write_pov(rep,"ch4.pov");
+run_povray("ch4");
+
+mol = mol_readxyz("h2o.xyz");
+rep = representation_();
+rep = mol_ball(mol,rep,{"O","H"},:,-0.2);
+rep = mol_ball(mol,rep,"X.?",1,-0.4);
+rep = rep_setdefaultscene_view3dscene(rep,[7.70,10.93,10.33],[1.00,-0.07,0.04],[-0.04,0.01,1.00],:,:,:,0);
+rep_write_obj(rep,"h2o.obj");
+rep_write_pov(rep,"h2o.pov");
+run_povray("h2o");
+
